56935017a1384e0e6bb3c6f6b20ec76662871815
Quy <quyp@pm.me>
2019-11-23 11:06:01 -0500

    update

---
+++ README.md
@@ -0,0 +1,24 @@
+# Airbnb project 
+![](https://images.unsplash.com/photo-1534430480872-3498386e7856?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2100&q=80)Credit: [Oliver Niblett](https://unsplash.com/@ojnibl)
+
+## Introduction
+
+Project consists of airbnb listings in New York City from 2016 to 2019 and the property values around the area. We want to see if Airbnb is contributing to a rise in property values in New york. There are multiple report claiming that Airbnb is damaging to the real estate market in large cities which pushes for legistlators to inact rent control within their respective city. 
+
+## Inspring visualizations
+
+https://i.ibb.co/xHJp9Lw/Screenshot-20191116-142932.png
+
+https://i.ibb.co/dKM37Tg/results-29-1.png
+
+https://i.ibb.co/vL0LphY/region-listings-time.png
+
+## Data Sources
+[NYC OpenData](https://data.cityofnewyork.us/City-Government/Revised-Notice-of-Property-Value-RNOPV-/8vgb-zm6e)
+
+[Inside Airbnb](http://insideairbnb.com/get-the-data.html) (.csv)
+
+## Sketch
+https://i.ibb.co/GMF535Z/results-36-0.png
+
+## Pipeline
---
+++ sql/schema.sql
@@ -0,0 +1,35 @@
+CREATE TABLE "airbnb2016" (
+	"id"                  TEXT     NOT NULL,
+    "neighbourhood_group" TEXT     NOT NULL,
+    "neighbourhood"       TEXT     NOT NULL,
+    "latitude"	          NUMERIC  NOT NULL,
+    "longitude"	          NUMERIC  NOT NULL,
+    "price"               NUMERIC  NOT NULL	
+);
+
+CREATE TABLE "airbnb2017" (
+	"id"	              TEXT     NOT NULL,
+    "neighbourhood_group" TEXT     NOT NULL,
+    "neighbourhood"       TEXT     NOT NULL,
+    "latitude"	          NUMERIC  NOT NULL,
+    "longitude"	          NUMERIC  NOT NULL,
+    "price"               NUMERIC  NOT NULL	
+);
+
+CREATE TABLE "airbnb2018" (
+	"id"	              TEXT     NOT NULL,
+    "neighbourhood_group" TEXT     NOT NULL,
+    "neighbourhood"       TEXT     NOT NULL,
+    "latitude"	          NUMERIC  NOT NULL,
+    "longitude"	          NUMERIC  NOT NULL,
+    "price"               NUMERIC  NOT NULL	
+);
+
+CREATE TABLE "airbnb2019" (
+	"id"	              TEXT    NOT NULL,
+    "neighbourhood_group" TEXT    NOT NULL,
+    "neighbourhood"       TEXT    NOT NULL,
+    "latitude"	          NUMERIC NOT NULL,
+    "longitude"	          NUMERIC NOT NULL,
+    "price"               NUMERIC NOT NULL	
+);
