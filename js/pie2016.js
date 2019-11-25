31e1ad11f647d63665256e9508dbf21dc2f77189
Quy <quyp@pm.me>
2019-11-25 14:28:15 -0500

    test

--- js/pie2016.js
+++ js/pie2016.js
@@ -1,4 +1,4 @@
-FusionCharts.ready(function() {
+`FusionCharts.ready(function() {
     var revenueChart = new FusionCharts({
       type: 'pie2d',
       renderAt: 'chart-container',
@@ -7,9 +7,8 @@ FusionCharts.ready(function() {
       dataFormat: 'json',
       dataSource: {
         "chart": {
-          "caption": "Number of listings in NYC",
-          "subCaption": "Last 2016",
-          "numberPrefix": "$",
+          "caption": "Number of NYC listings in 2016",
+          "subCaption": "2016",
           "showPercentInTooltip": "0",
           "decimals": "1",
           //Theme
@@ -42,4 +41,49 @@ FusionCharts.ready(function() {
       }
     }).render();
   
-  });
+  });`
+
+  `FusionCharts.ready(function() {
+    var revenueChart = new FusionCharts({
+      type: 'pie2d',
+      renderAt: 'chart-container',
+      width: '550',
+      height: '350',
+      dataFormat: 'json',
+      dataSource: {
+        "chart": {
+          "caption": "Number of NYC listings in 2017",
+          "subCaption": "2017",
+          "showPercentInTooltip": "0",
+          "decimals": "1",
+          //Theme
+          "theme": "fusion",
+          "valuePosition": "inside",
+          "textOutline": "1"
+        },
+        "data": [{
+            "label": "Bronx",
+            "value": "7304"
+          },
+          {
+            "label": "Brooklyn",
+            "value": "178550"
+              
+          },
+          {
+            "label": "Manhattan",
+            "value": "202048"
+          },
+          {
+            "label": "Queens",
+            "value": "42337"
+          },
+          {
+            "label": "Staten Island",
+            "value": "2798"
+          }
+        ]
+      }
+    }).render();
+  
+  });`
